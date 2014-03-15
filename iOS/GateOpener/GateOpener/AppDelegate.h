a856044ef895b47276dc7008f1f8395488e56153
Mark Puddick <mark@moroku>
2014-03-15 17:45:45 +1100

    Initial Commit

---
+++ iOS/GateOpener/GateOpener/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GateOpener
+//
+//  Created by MARK PUDDICK on 15/03/2014.
+//  Copyright (c) 2014 Invoax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/GateOpener/GateOpener/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GateOpener
+//
+//  Created by MARK PUDDICK on 15/03/2014.
+//  Copyright (c) 2014 Invoax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
